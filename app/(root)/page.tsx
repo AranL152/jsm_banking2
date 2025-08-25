9406abd762f032221d150d3b2f6690a3b1ee5cc5
AranL152 <aranlum1121@gmail.com>
2025-08-25 01:06:35 +0000

    new

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import "../globals.css"
+
 
 const Home = () => {
+
+    const loggedIn = { firstName: 'Adrian'}
   return (
-    <div>Home</div>
+    <section className = "home">
+        <div className="home-content"> 
+            <header className="home-header">
+                <HeaderBox
+                 type="greeting"
+                 title="Welcome"
+                 user={loggedIn?.firstName || 'Guest'}
+                 subtext='Access and Manage your account and transactions efficiently'
+                />
+
+                <TotalBalanceBox 
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1250.35}
+                />
+            </header>
+
+        </div>
+    </section>
   )
 }
 
